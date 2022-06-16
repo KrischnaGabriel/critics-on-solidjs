7578327c31d59016292fcfb0375d692401e69a4c
Krischna Gabriel <39499729+KrischnaGabriel@users.noreply.github.com>
2022-06-16 10:07:27 +0200

    Update App3.tsx

--- solid-app2/src/App3.tsx
+++ solid-app2/src/App3.tsx
@@ -63,7 +63,7 @@ export default function() {
 
 /*
 This does work (but ironically only with nested objects, cuz my selfmade hook isn't so well designed),
-And there is no limit on the amount/depth of nested objects. 
+And there is no limit on the depth of nested objects. 
 ```
 >> obj.val = {a: 1, b:0}
 >> obj.val = {a: 1, b:1}
